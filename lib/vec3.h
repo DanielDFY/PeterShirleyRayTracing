0aa6ddaab5193cf8eacde97ccf8dcec26f562cce
Daniel <daniel_dfy98@163.com>
2019-02-12 23:51:21 +0800

    Update vec3.h

--- lib/vec3.h
+++ lib/vec3.h
@@ -110,7 +110,7 @@ inline vec3& vec3::operator*=(const float t) {
 }
 
 inline vec3& vec3::operator/=(const float t) {
-    float k = 1.0/t;
+    float k = (float)1.0/t;
 
     e[0]  *= k;
     e[1]  *= k;
@@ -129,7 +129,7 @@ inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
 }
 
 inline void vec3::make_unit_vector() {
-    float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+    float k = (float)1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
     e[0] *= k; e[1] *= k; e[2] *= k;
 }
 
