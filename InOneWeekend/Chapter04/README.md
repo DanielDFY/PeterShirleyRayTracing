1ee9d89b45209ff6ace643df975b049ca6764770
DanielDFY <daniel_dfy98@163.com>
2020-06-06 17:58:36 +0800

    update chapter 05

---
+++ InOneWeekend/Chapter04/README.md
@@ -0,0 +1,22 @@
+# Chapter 04
+
+### Ray-Sphere Intersection
+
+(x - C<sub>x</sub>)<sup>2</sup> + (y - C<sub>y</sub>)<sup>2</sup> + (z - C<sub>z</sub>)<sup>2</sup> = r<sup>2</sup>
+
+<=> (**P** - **C**) &sdot; (**P** - **C**) = r<sup>2</sup>
+
+<=> (**A** + t**b** - C) &sdot; (**A** + t**b** - C) = r<sup>2</sup>
+
+<=> t<sup>2</sup>**b**&sdot;**b** + 2t**b** &sdot; (A - C) + (A - C) &sdot; (A - C) - r<sup>2</sup> = 0
+
+![fig.ray-sphere](note images/fig.ray-sphere.jpg)
+
+
+
+### Output Display
+
+A simple red sphere
+
+![output](output.png)
+
---
+++ InOneWeekend/Chapter04/main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <fstream>
+
+#include <Color.h>
+#include <Ray.h>
+
+#define PPM	// produce output.ppm
+#define PNG	// produce output.png
+
+#ifdef PNG
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
+#endif
+
+bool hitSphere(const Point3& center,const double radius, const Ray& r) {
+	const Vec3 oc = r.origin() - center;
+	const double a = dot(r.direction(), r.direction());
+	const double b = 2.0 * dot(oc, r.direction());
+	const double c = dot(oc, oc) - radius * radius;
+	const double discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
+
+Color rayColor(const Ray& r) {
+	if (hitSphere(Point3(0.0, 0.0, -1.0), 0.5, r)) {
+		return {1.0, 0.0, 0.0};
+	}
+	
+	const Vec3 unitDirection = unitVec3(r.direction());
+	const double t = 0.5 * (unitDirection.y() + 1.0);
+	return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+}
+
+int main() {
+	/* image config */
+	constexpr double aspectRatio = 16.0 / 9.0;
+	constexpr int imageWidth = 384;
+	constexpr auto imageHeight = static_cast<int>(imageWidth / aspectRatio);
+
+	/* image output file */
+	const std::string fileName("output");
+
+	#ifdef PPM
+	// preparation
+	const std::string ppmFileName(fileName + ".ppm");
+	std::ofstream outputStream(ppmFileName);
+	if (!outputStream.is_open()) {
+		std::cerr << "failed to open file " << ppmFileName << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// .ppm header
+	outputStream << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+	#endif
+
+	#ifdef PNG
+	// preparation
+	const std::string pngFileName(fileName + ".png");
+	constexpr int channelNum = 3;
+	constexpr int imageSize = imageWidth * imageHeight * channelNum;
+	constexpr size_t strideInBytes = imageWidth * channelNum * sizeof(unsigned char);
+
+	const std::unique_ptr<unsigned char[]> pixelDataPtr(new unsigned char[imageSize]);
+	int idx = 0;
+	#endif
+
+	/* camera config */
+	constexpr double viewPortHeight = 2.0;
+	constexpr double viewPortWidth = aspectRatio * viewPortHeight;
+	constexpr double focalLength = 1.0;
+
+	const Point3 origin(0.0, 0.0, 0.0);
+	const Vec3 horizontal(viewPortWidth, 0.0, 0.0);
+	const Vec3 vertical(0.0, viewPortHeight, 0.0);
+	const Point3 lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - Vec3(0.0, 0.0, focalLength);
+	
+	/*
+	 * the pixels are written out in rows with pixels left to right.
+	 * the rows are written out from top to bottom.
+	 */
+	for (int row = 0; row < imageHeight; ++row) {
+		// print progress info during processing
+		printf("\rProcessing[%.2lf%%]", static_cast<double>(row * 100.0) / static_cast<double>(imageHeight - 1));
+
+		for (int col = 0; col < imageWidth; ++col) {
+			auto u = static_cast<double>(col) / static_cast<double>(imageWidth - 1);
+			auto v = static_cast<double>(row) / static_cast<double>(imageHeight - 1);
+			
+			Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+
+			Color color = rayColor(r);
+			
+			#ifdef PPM
+			// write pixel data to output.ppm
+			writeColor8bit(outputStream, color);
+			#endif
+
+			#ifdef PNG
+			const auto ucr = static_cast<unsigned char>(color.r8bit());
+			const auto ucg = static_cast<unsigned char>(color.g8bit());
+			const auto ucb = static_cast<unsigned char>(color.b8bit());
+			// store pixel data
+			pixelDataPtr[idx++] = ucr;
+			pixelDataPtr[idx++] = ucg;
+			pixelDataPtr[idx++] = ucb;
+			#endif
+		}
+	}
+
+	// print complete info
+	printf("\rProcessing[100.00%%]\nComplete!\n");
+
+	#ifdef PPM
+	// close output.ppm
+	outputStream.close();
+	#endif
+
+	#ifdef PNG
+	// write pixel data to output.png
+	stbi_write_png(pngFileName.c_str(), imageWidth, imageHeight, channelNum, pixelDataPtr.get(), strideInBytes);
+	#endif
+
+	return 0;
+}
